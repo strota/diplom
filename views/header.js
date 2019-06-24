57a86d0d22b6291293728077ed8b8e7ef02125d0
strota <32236607+strota@users.noreply.github.com>
2019-06-24 19:41:41 +0500

    Create header.js

---
+++ views/header.js
@@ -0,0 +1,9 @@
+<header>
+    <form method="post" action="/">
+        <input name="id_man">
+        <button>Search</button>
+
+    </form>
+    <a href="https://oauth.vk.com/authorize?client_id=7020229&display=page&redirect_uri=http://localhost:3000/auth&scope=friends&response_type=code&v=5.95">Login</a>
+    <a href="/menu/help"> Help</a>
+</header>
